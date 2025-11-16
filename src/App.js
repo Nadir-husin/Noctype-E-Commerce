ee3a7ebdffe557c34c1604c3ab469d0db40afb74
Nader <eng.nadirr@gmail.com>
2025-11-17 00:12:25 +0200

    applied some changes on app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Routes/Home';
 import Products from './Routes/Products';
 import About from './Routes/About';
@@ -11,61 +11,46 @@ import Footer from './components/Footer';
 import ProductDetail from './components/Productdetail';
 import { ProductsProvider } from './contexts/ProductsContext';
 import { CartProvider } from './contexts/CartContext';
-import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-// component
 import Navbar from './components/Navbar';
 import Checkout from './Routes/Checkout';
 
-
-
 function App() {
 
+  useEffect(() => {
+    AOS.init({ duration: 800, once: true });
+  }, []);
 
-            useEffect(() => {
-              AOS.init({
-                duration: 800,
-                once: true,
-              });
-            }, []); 
-      
-            useEffect(()=>{
-              document.title = "Noctype E-Commerce "
-            },[])
-            
+  useEffect(() => {
+    document.title = "Noctype E-Commerce ";
+  }, []);
+
   return (
-
-    <>
+    <BrowserRouter basename="/Noctype-E-Commerce">
       <ProductsProvider>
         <CartProvider>
+
           <Navbar />
 
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:productId" element={<ProductDetail />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
 
-          
-    <Router basename="/Noctype-E-Commerce">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:productId" element={<ProductDetail />} /> 
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
+          <Footer />
 
-      </Routes>
-    </Router>
-
-
-    <Footer />
-    </CartProvider>
-    </ProductsProvider>
-
-    </>
-
+        </CartProvider>
+      </ProductsProvider>
+    </BrowserRouter>
   );
 }
 
